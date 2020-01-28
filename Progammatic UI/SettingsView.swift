9c7be480481269124936d5e904eb668716acf105
Tsering Lama <tseringlama@pursuit.org>
2020-01-28 14:07:42 -0500

    attempting to create a setting view

---
+++ Progammatic UI/SettingsView.swift
@@ -0,0 +1,28 @@
+//
+//  SettingsView.swift
+//  Progammatic UI
+//
+//  Created by Tsering Lama on 1/28/20.
+//  Copyright © 2020 Tsering Lama. All rights reserved.
+//
+
+import UIKit
+
+class SettingsView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        
+    }
+    
+    
+}
